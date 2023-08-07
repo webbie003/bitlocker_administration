efb51ce064d574016856f559b4dbba0ec7f90dcb
webbie003 <webbie_003@hotmail.com>
2023-08-07 14:21:56 +1000

    Add files via upload

---
+++ BitLocker - Check AD Recovery Code Creation.ps1
@@ -0,0 +1,8 @@
+﻿$objComputer = Get-ADComputer -Filter * -Properties lastLogon,OperatingSystem # | Where-Object {$_.OperatingSystem -clike "Windows 10*"}
+$output = @('Workstation Name,Last Logon (Data/Time),Operating System,Recovery Code 1,Recovery Code 2,Recovery Code 3,Recovery Code 4')
+ForEach ($computer in $objComputer){
+    $recoveryInfo = Get-ADObject -Filter 'objectClass -eq "msFVE-RecoveryInformation"' -SearchBase $computer.DistinguishedName -Properties whenCreated,msFVE-RecoveryPassword | Select @{N='LastLogon'; E={[DateTime]::FromFileTime($_.LastLogon)}},OperatingSystem,whenCreated,msFVE-RecoveryPassword
+    If ($recoveryInfo.whenCreated){$rIwC = ($recoveryInfo.whenCreated) -join "," }else{$rIwC = "None"}
+    $output += $computer.Name + "," + [DateTime]::FromFileTime($computer.LastLogon) + "," + $computer.OperatingSystem + "," + $rIwC
+}
+$output > ((Get-Variable HOME -valueOnly) + "\Desktop\" + $env:USERDOMAIN + "_BitLocker.csv")
---
+++ Bitlocker - Backup Recovery Keys.ps1
@@ -0,0 +1,9 @@
+﻿#$bldrives = Get-PSDrive -PSProvider FileSystem | Where-Object {$_.Used -ne ""} 
+$bldrives = Get-BitLockerVolume
+$noEncrypt = Get-PSDrive -PSProvider FileSystem | Where-Object {($_.Used -ne "") } 
+If ((Test-Path -Path ("C:\temp\"+($env:computername)+".txt")) -eq $false) {
+    Foreach ($drive in $bldrives) {
+        # Write-Host $drive.MountPoint
+        Manage-Bde -protectors -get $drive.MountPoint | Out-File -Append ("C:\temp\"+($env:computername)+".txt")
+    }
+}
